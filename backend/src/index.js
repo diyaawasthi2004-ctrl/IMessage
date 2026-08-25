bb478066ac2ac33ae28ee403e290c49533277faa
diyaawasthi2004-ctrl <diyaawasthi14@gmail.com>
2026-08-25 13:56:14 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config";
+
+
+
+
+const app = express();
+const PORT = process.env.PORT
+
+
+
+app.listen(PORT, () => console.log("Server is up and running on PORT:", PORT));
